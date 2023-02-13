3ad70a7eaddc415cc0f2b9af7043472dc5479df9
NDT1409 <124195943+NDT1409@users.noreply.github.com>
2023-02-13 01:30:51 +0100

    Homework 2

---
+++ Task1.java
@@ -0,0 +1,23 @@
+package home;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        char c = 'G';
+        int i = 89;
+        byte b = 4;
+        short s = 56;
+        float f = 4.7333436F;
+        double d = 4.35453532;
+        long l = 12121;
+
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(l);
+    }
+}
